bda7ff6afe40ea249beea8d73c2ab23a2d16e0ab
Sandro Santilli <strk@keybit.net>
2010-09-09 16:17:49 +0200

    Demonstrate how to do some initialization in style plugin class constructor and how to hide private methods so they don't fall in final styleMap context

--- tests/plugins/style_plugin/openlayers_test_test_plugin.js
+++ tests/plugins/style_plugin/openlayers_test_test_plugin.js
@@ -10,6 +10,8 @@
  */
 Drupal.openlayers.style_plugin.openlayers_test_test_plugin = function (params) {
   this.params = params;
+  this.params.point_radius_high = parseInt(this.params.point_radius_high);
+  this.params.point_radius_low = parseInt(this.params.point_radius_low);
 };
 
 /**
@@ -17,17 +19,24 @@ Drupal.openlayers.style_plugin.openlayers_test_test_plugin = function (params) {
  */
 Drupal.openlayers.style_plugin.openlayers_test_test_plugin.prototype = {
 
+  // Private methods (not copied to final style context object)
+  'prv' : {
+    'random' : function(low, high) {
+      return Math.floor(Math.random() * (high-low+1)) + low;
+    }
+  },
+  
   // Fill opacity context.  Sets random fill opacity.
   'getFillOpacity' : function(feature) {
     // Random fill opacity
-    return Math.random();
+    return this.prv.random(1, 100)/100;
   },
   
   // Point radius context.  Given paramters, gets a random
   // pointRadius.
   'getPointRadius' :  function(feature) {
-    var high = parseInt(this.params.point_radius_high);
-    var low = parseInt(this.params.point_radius_low);
-    return Math.floor(Math.random() * (high-low+1)) + low
+    var high = this.params.point_radius_high;
+    var low = this.params.point_radius_low;
+    return this.prv.random(low, high);
   }
 };
