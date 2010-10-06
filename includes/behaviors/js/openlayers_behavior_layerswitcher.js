965a5c65b8e5c08d21d602d67213e271a7c9fd41
Sandro Santilli <strk@keybit.net>
2010-10-06 16:31:22 +0200

    cvs up

--- includes/behaviors/js/openlayers_behavior_layerswitcher.js
+++ includes/behaviors/js/openlayers_behavior_layerswitcher.js
@@ -1,4 +1,4 @@
-// $Id: openlayers_behavior_layerswitcher.js,v 1.1.2.6 2010/10/06 09:06:34 strk Exp $
+// $Id: openlayers_behavior_layerswitcher.js,v 1.1.2.7 2010/10/06 14:31:11 strk Exp $
 
 /**
  * @file
@@ -15,7 +15,7 @@ Drupal.behaviors.openlayers_behavior_layerswitcher = function(context) {
     var control = new OpenLayers.Control.LayerSwitcher({
       'ascending': !!data.map.behaviors['openlayers_behavior_layerswitcher'].ascending,
       'roundedCorner' : !!data.map.behaviors['openlayers_behavior_layerswitcher'].roundedCorner,
-      'roundedCornerColor' : data.map.behaviors['openlayers_behavior_layerswitcher'].roundedCornerColor,
+      'roundedCornerColor' : data.map.behaviors['openlayers_behavior_layerswitcher'].roundedCornerColor
     });
     data.openlayers.addControl(control);
     control.activate();
