3c069305aaca5f389de4b9c4083e5fbd2369c18a
Sandro Santilli <strk@keybit.net>
2010-10-22 11:13:48 +0200

    cvs up

--- tests/plugins/style_plugin/openlayers_test_rnd_factor.js
+++ tests/plugins/style_plugin/openlayers_test_rnd_factor.js
@@ -1,4 +1,4 @@
-// $Id: openlayers_test_rnd_factor.js,v 1.1.2.1 2010/10/13 09:03:58 strk Exp $
+// $Id: openlayers_test_rnd_factor.js,v 1.1.2.2 2010/10/13 13:35:35 tmcw Exp $
 
 /**
  * @file
@@ -8,7 +8,7 @@
 /**
  * Style plugin context class
  */
-Drupal.openlayers.style_plugin.openlayers_test_rnd_factor = function (params) {
+Drupal.openlayers.style_plugin.openlayers_test_rnd_factor = function(params) {
   this.params = params;
 };
 
